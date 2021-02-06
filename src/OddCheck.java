07671ac584726d230ff8b018dcb469274903d358
Java <Java@10.0.0.70>
2021-02-06 16:21:55 -0500

    Initial Load.

---
+++ src/OddCheck.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class OddCheck {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Type an integer Value");
+
+		while (true) {
+			try {
+
+				String value = input.nextLine();
+				int i = Integer.parseInt(value);
+
+				if (i % 2 == 0) {
+					System.out.println("Even");
+				} else {
+					System.out.println("Odd");
+				}
+				System.out.println("\nEnter another integer value if you wish to continue.");
+
+			} catch (Exception e) {
+				System.out.println("\nPlease enter a proper integer value");
+			}
+		}
+
+	}
+}
